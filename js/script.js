584fd026f6a750355a6d067145e47ebdf9fcdc46
Jai Prakash Patel <prakashjaipatel1@gmail.com>
2024-02-24 19:31:17 +0530

    upload new content

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// tonggle 
+let a=document.getElementById("menu");
+function show(){
+    // document.getElementById("tonggle").click( document.getElementById("menu").style.display="block");
+    if(a.style.display=="block"){
+        a.style.display="none";
+    }
+    else{
+        a.style.display="block";
+    }
+}
+
